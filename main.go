60362e1b2429ea033a97a7ba3fa700d77d9c6072
Riku Kestilä <rikukestila@gmail.com>
2022-11-14 10:21:41 +0200

    Use goroutines in server calls

--- main.go
+++ main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 	"time"
 )
 
-type sodexoLunchCli struct {
-	apiBase string
-}
+var client = http.Client{}
+
+const apiBase = "https://www.sodexo.fi/ruokalistat/output/daily_json/"
 
 type Course struct {
 	Title     string `json:"title_fi"`
@@ -24,18 +26,25 @@ type LunchInfo struct {
 	Courses map[int]Course `json:"courses"`
 }
 
-func (s sodexoLunchCli) getLunch(restaurantUrl string) error {
-	resp, err := http.Get(s.apiBase + restaurantUrl)
+func getLunch(ctx context.Context, label, restaurantUrl string) error {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, restaurantUrl, nil)
+
 	if err != nil {
+		fmt.Println(label, "request err:", err)
 		return err
 	}
-	defer resp.Body.Close()
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(label, "response err:", err)
+		return err
+	}
 
 	var data struct {
 		LunchInfo
 	}
 
 	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		fmt.Println(label, "decoding err:", err)
 		log.Fatal(err)
 		return err
 	}
@@ -47,11 +56,40 @@ func (s sodexoLunchCli) getLunch(restaurantUrl string) error {
 	return nil
 }
 
+func callAll(ctx context.Context, r1, r2 string) {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		err := getLunch(ctx, "sodexo5", r1)
+		if err != nil {
+			cancel()
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		err := getLunch(ctx, "sodexo6", r2)
+		if err != nil {
+			cancel()
+		}
+	}()
+
+	wg.Wait()
+	fmt.Println("done with both")
+}
+
 func main() {
 	now := time.Now()
 	todayString := now.Format("2006-01-02")
 	fmt.Println(todayString)
-	lc := sodexoLunchCli{apiBase: "https://www.sodexo.fi/ruokalistat/output/daily_json/"}
-	lc.getLunch("107/" + todayString)
-	lc.getLunch("110/" + todayString)
+
+	ctx := context.Background()
+	s5Url := apiBase + "107/" + todayString
+	s6Url := apiBase + "110/" + todayString
+
+	callAll(ctx, s5Url, s6Url)
 }
